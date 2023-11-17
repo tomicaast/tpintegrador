78c9ed6984452d8c63d262d129d7007814d49e2e
lucasaudouin <laudouin@udesa.edu.ar>
2023-11-16 23:35:53 -0300

    Modifico js para pelis valoradas e index.html

--- js/index.js
+++ js/index.js
@@ -106,3 +106,57 @@ fetch(seriesPopulares)
     .catch(function(error) {
         console.log("Error al obtener la información: " + error);
     });
+let pelisValoradas = `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}`;
+    fetch(pelisValoradas)
+    .then(function(response) {
+        return response.json();
+    })
+
+
+    .then(function(data) {
+        console.log(data);
+        let results = data.results;
+        let fotos = ``;
+        let contenedor = document.querySelector("#pvalo");
+        let modifContenedor = ``
+        let peliculasRandom = [];
+    
+        while (peliculasRandom.length < 6) {
+            let peliId = Math.floor(Math.random() * results.length);
+            let repetido = false;
+    
+            for (let i = 0; i < peliculasRandom.length; i++) {
+                if (peliculasRandom[i] === peliId) {
+                    repetido = true;
+                    break;
+                }
+            }
+            if (!repetido) {
+                peliculasRandom.push(peliId);
+            }
+        }
+
+        for (let i = 0; i < 6; i++) {
+            let peliId = peliculasRandom[i];
+            let movie_id = results[peliId].id;
+            let movie_title = results[peliId].title;
+            let fecha = results[peliId].release_date;
+            let posterPath = results[peliId].poster_path
+            let poster = "https://image.tmdb.org/t/p/w200" + posterPath;
+                modifContenedor += `
+                <article class="peli">
+                <h3 class="titulo peli">${movie_title}</h3>
+                <p>Fecha de estreno: ${fecha}</p>
+                <img src="${poster}" alt="${movie_title}" class="pelimg">
+                <a href="detail-movie.html"><button type="submit" class="boton">Ver detalle</button></a>
+                </article>
+                `
+            }
+                contenedor.innerHTML = modifContenedor;
+
+            }
+
+    )
+    .catch(function(error) {
+        console.log("Error al obtener la información: " + error);
+    })
